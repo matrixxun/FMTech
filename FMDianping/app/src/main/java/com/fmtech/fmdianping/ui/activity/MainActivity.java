26017441d8cdb59b298aa3f9a33b15a87f78eced
Drew.Chiang <jiangchuna@126.com>
2015-11-08 21:44:56 +0800

    Accomplished the MainActivity's bottom tabs.

--- FMDianping/app/src/main/java/com/fmtech/fmdianping/ui/activity/MainActivity.java
+++ FMDianping/app/src/main/java/com/fmtech/fmdianping/ui/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fmtech.fmdianping.ui.activity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,24 +11,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.fmtech.fmdianping.R;
+import com.fmtech.fmdianping.ui.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        setContentView(R.layout.content_main);
     }
 
     @Override
---
+++ FMDianping/app/src/main/java/com/fmtech/fmdianping/ui/fragment/HomeFragment.java
@@ -0,0 +1,40 @@
+package com.fmtech.fmdianping.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2015 FMTech All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ * @email chiangchuna@gmail.com
+ * @create_date 2015/11/8 20:42
+ * @description ${todo}
+ * <p/>
+ * Modification History:
+ * Date            Author            Version         Description
+ * -----------------------------------------------------------------
+ * 2015/11/8 20:42  Drew.Chiang       v1.0.0          create
+ * <p/>
+ * ==================================================================
+ */
+
+public class HomeFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
