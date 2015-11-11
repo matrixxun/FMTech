34e3dd491d4a4ac038319f7fe9e8fc7f632acff7
Drew.Chiang <jiangchuna@126.com>
2015-11-11 22:07:56 +0800

    Add CrashReportHelper.

---
+++ FMDianping/app/src/main/java/com/fmtech/fmdianping/util/CrashReportHelper.java
@@ -0,0 +1,23 @@
+package com.fmtech.fmdianping.util;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2015 FMTech All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ * @email chiangchuna@gmail.com
+ * @create_date 2015/11/11 22:04
+ * @description ${todo}
+ * <p/>
+ * Modification History:
+ * Date            Author            Version         Description
+ * -----------------------------------------------------------------
+ * 2015/11/11 22:04  Drew.Chiang       v1.0.0          create
+ * <p/>
+ * ==================================================================
+ */
+
+public class CrashReportHelper {
+
+}
